b86a417d7bf7b8a541956defa43ef632a617436e
杜苗增(Miaozeng Du) <dmzhhu@126.com>
2024-11-13 13:00:26 +0800

    1

---
+++ LLaVA-main/scripts/v1_5/finetune_lora.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+deepspeed --include="localhost:0,6,4" /data2/dmz/llava_test/LLaVA-main/llava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed /data2/dmz/llava_test/LLaVA-main/scripts/zero3.json \
+    --model_name_or_path /data2/dmz/llava_test/LLaVA-main/llava-v1.5-7b \
+    --version v1 \
+    --data_path /data2/dmz/llava_test/LLaVA-main/elonga.json \
+    --image_folder /data2/dmz/llava_test/LLaVA-main/all_pic/elonmusk \
+    --vision_tower /data2/dmz/llava_test/LLaVA-main/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir /data2/dmz/llava_test/LLaVA-main/checkpoints/llava-v1.5-7b-lora-Elon-2 \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
